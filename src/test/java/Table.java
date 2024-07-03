0059be5bf4637d6808e77b806ca7b6dd5bf55a0f
Katythecat <kk.wilasinee@gmail.com>
2024-07-03 11:00:10 -0700

    update

--- src/test/java/Table.java
+++ src/test/java/Table.java
@@ -1,10 +1,15 @@
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 public class Table {
     public static void main(String[] args) {
-        WebDriver driver = new ChromeDriver();
+       // WebDriver driver = new ChromeDriver();
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--headless");
+        WebDriver driver = new ChromeDriver(options);
+
         driver.manage().window().maximize();
         driver.get("http://hrm.syntaxtechs.net/humanresources/symfony/web/index.php/auth/login");
 //        login
@@ -13,6 +18,7 @@ public class Table {
         driver.findElement(By.id("txtPassword")).sendKeys("Hum@nhrm123");
 
         driver.findElement(By.id("btnLogin")).click();
+        System.out.println("test poll");
 
 //click on PIm option
         driver.findElement(By.id("menu_pim_viewPimModule")).click();
